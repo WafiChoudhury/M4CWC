dbe3ce0ec73e2572c39d10b885968dc235a3a551
Wafi Choudhury <wafster1337@gmail.com>
2021-07-12 16:46:19 -0500

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Wafi Choudhury on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
